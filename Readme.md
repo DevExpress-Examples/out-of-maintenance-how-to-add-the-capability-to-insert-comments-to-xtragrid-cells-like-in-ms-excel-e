f5bb9a2f4c8c96a99eb4476cfa5ba5ddd5a3544f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to add the capability to insert comments to XtraGrid cells (like in MS Excel)
+
+
+<p>In this example, the following functionality is implemented:<br />
+1. It is possible to insert a comment to an XtraGrid cell via the context menu item. This comment is stored in a separate storage (Dictionary).<br />
+2. It is possible to edit a comment via the context menu item.<br />
+3. When a cell has a comment, a triangle is drawn in the top right corner of this cell.<br />
+4. When the user moves the mouse pointer to a cell with a comment, a hint with the text of this comment is shown.</p>
+
+<br/>
+
+
---
+++ VB/WinFormsExample/Class1.vb
@@ -0,0 +1,36 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace WinFormsExample
+	Public Class TestClass
+		Private _Property1 As String
+		Public Property Property1() As String
+			Get
+				Return _Property1
+			End Get
+			Set(ByVal value As String)
+				_Property1 = value
+			End Set
+		End Property
+		Private _Property2 As String
+		Public Property Property2() As String
+			Get
+				Return _Property2
+			End Get
+			Set(ByVal value As String)
+				_Property2 = value
+			End Set
+		End Property
+		Private _Property3 As String
+		Public Property Property3() As String
+			Get
+				Return _Property3
+			End Get
+			Set(ByVal value As String)
+				_Property3 = value
+			End Set
+		End Property
+	End Class
+End Namespace
